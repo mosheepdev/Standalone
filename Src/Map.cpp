d4e784b7835f918746f5ce81d3b89d9fcbfd68ee
Abit Gray <AbitTheGray@gmail.com>
2018-09-26 12:07:31 +0200

    Map C++ class progress

--- Src/Map.cpp
+++ Src/Map.cpp
@@ -1,5 +1,11 @@
-//
-// Created by abit on 26.9.18.
-//
-
 #include "Map.h"
+
+Map::Map(string name, unsigned int width, unsigned int height) : _Name(name), _Width(width), _Height(height)
+{
+    // Clear Layer Textures (ID + Weight)
+    for(unsigned char i = 0; i < MAP_LAYER_MAX; i++)
+    {
+        _LayerTextureId[i] = -1;
+        _LayerTextureWeight[i] = 0.0f;
+    }
+};
--- Src/Map.h
+++ Src/Map.h
@@ -12,14 +12,31 @@ public:
     ~Map();
     Map(Map& map);
 
+private:
+    string _Name;
+public:
+    string GetName() { return _Name; }
+
+private:
+    unsigned int _Width, _Height;
+public:
+    unsigned int GetWidth() { return _Width; }
+    unsigned int GetHeight() { return _Height; }
+
 public:
     signed char GetHeight(int width, int height);
 
 // Texture
-public:
-    int GetTextureId(unsigned char layerId);
-    void SetTextureId(unsigned char layerId, int textureId);
 #define MAP_LAYER_MAX 8
+public:
+    int GetTextureId(unsigned char layerId) { return _LayerTextureId[layerId]; };
+    float GetTextureWeight(unsigned char layerId) { return _LayerTextureWeight[layerId]; };
+    void SetTextureId(unsigned char layerId, int textureId);
+    void SetTextureWeight(unsigned char layerId, float textureWeight);
+    void SetTexture(unsigned char layerId, int textureId, float textureWeight);
+private:
+    int _LayerTextureId[MAP_LAYER_MAX];
+    float _LayerTextureWeight[MAP_LAYER_MAX];
 };
 
 
